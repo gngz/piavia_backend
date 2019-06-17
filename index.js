babaa11a44587bf563d0a2fb2fc4a1dcea6c08fd
Gonçalo Passos <social@diogopassos.pt>
2019-06-17 20:54:46 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const userController = require("./lib/user");
+const adsb = require("./lib/adsb");
+
+const express = require('express')
+const app = express()
+const port = 9090;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/adsb', (req, res) => res.send(adsb.getFlights()))
+app.get('/history', (req, res) => res.send(adsb.getHistory()))
+app.get('/history/:hex', function (req, res) {
+    res.send(adsb.getHistoryByFlight(req.params.hex))
+});
+
+app.listen(port, () => {});
+
+
+console.log("Welcome to PiAvia")
